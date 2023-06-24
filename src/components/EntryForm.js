5fbe285807c6cbaae55748a0b774af11e4b9c3fa
Ekaterina Bruch <ekaterinabruch@gmail.com>
2023-06-24 22:01:53 +0200

    Style Header, Footer, Form

--- src/components/EntryForm.js
+++ src/components/EntryForm.js
@@ -1,12 +1,18 @@
 export default function EntryForm() {
   return (
-    <form>
-      <h2>NEW ENTRY</h2>
-      <label htmlFor="motto">Motto</label>
-      <input type="text" id="motto"></input>
-      <label htmlFor="notes">Notes</label>
-      <input type="text" id="notes"></input>
-      <button type="submit">Create</button>
+    <form className="form">
+      <h2 class="form__title">NEW ENTRY</h2>
+      <label htmlFor="motto" className="labelForm">
+        Motto
+      </label>
+      <input type="text" id="motto" className="MottoInput"></input>
+      <label htmlFor="notes" className="labelForm">
+        Notes
+      </label>
+      <input type="text" id="notes" className="NotesInput"></input>
+      <button type="submit" className="createButton">
+        Create
+      </button>
     </form>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
-    <header>
-      <h1>Journal</h1>
+    <header className="header">
+      <h1 className="title">JOURNAL</h1>
     </header>
   );
 }
